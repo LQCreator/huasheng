73675d31f892141739da55fb43b4e9ef28fdb4f9
OF2163 <lingqiang@qianmi.com>
2018-11-25 22:18:38 +0800

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu"
+      mode="horizontal"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+      @select="handleSelect">
+      <el-menu-item index="1">首页</el-menu-item>
+      <el-menu-item index="2">产品中心</el-menu-item>
+      <el-menu-item index="3">新闻资讯</el-menu-item>
+      <el-menu-item index="4">工程案例</el-menu-item>
+      <el-menu-item index="5">公司简介</el-menu-item>
+      <el-menu-item index="6">联系我们</el-menu-item>
+    </el-menu>
+    <div class="companyIcon">
+      <img
+        src="~/assets/company.jpg"
+        alt="公司名称">
+    </div>
+    <div class="block">
+      <el-carousel trigger="click" height="150px">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3>{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1'
+      }
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped="" type="text/css">
+  .el-menu {
+    padding-left: 60%;
+  }
+  .companyIcon {
+    width: 30%;
+    height: 60px;
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+
+  .companyIcon img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .el-carousel-item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 150px;
+    margin: 0;
+  }
+
+  .el-carousel-item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel-item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+</style>
