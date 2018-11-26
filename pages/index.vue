aadabe8d3667884caf2ac9121e69391933997041
OF2163 <lingqiang@qianmi.com>
2018-11-26 23:44:39 +0800

    首页部分代码

--- pages/index.vue
+++ pages/index.vue
@@ -35,28 +35,27 @@
           <h2>Product Center</h2>
         </div>
         <ul class="swiper-wrapper">
-          <li class="swiper-slide">
-            <a href="//bm001.com/product/ygl/"><i class="iconfont icon-guanli"></i>
-            <h3>陶粒</h3>
-            <p>提升效率，智能分析，老客户倍增</p>
+          <li class="swiper-slide" v-for="(product,index) in productList" :key="index">
+            <a href="//bm001.com/product/ywd/">
+              <img class="product-img" :src="product.url">
+              <h3>{{product.name}}</h3>
             </a>
           </li>
-          <li class="swiper-slide">
-            <a href="//bm001.com/product/ywd/"><i class="iconfont icon-weidian"></i>
-            <h3>岩棉板</h3>
-            <p>玩转微信社群流量，增长新客户</p>
-            </a></li>
-          <li class="swiper-slide">
-            <a href="//bm001.com/product/yls/"><i class="iconfont icon-liansuo"></i>
-            <h3>挤塑板</h3>
-            <p>门店加盟不烦忧，加盟渠道多又多</p>
-            </a>
-          </li>
-          <li class="swiper-slide">
-            <a href="//bm001.com/product/yay/"><i class="iconfont icon-ayi"></i>
-            <h3>陶粒加气块</h3>
-            <p>阿姨数量多，质量高，粘性强</p>
-          </a>
+        </ul>
+        <div class="swiper-pagination"></div>
+      </div>
+    </div>
+    <div class="case">
+      <div class="container swiper-container-sy">
+        <div class="title">
+          <p>南京华盛新型节能环保材料有限公司本着“服务与客户至上，打造地区陶粒实惠价”。热诚欢迎各界朋友前来参观、考察、洽谈业务。</p>
+          <h2>工程案例</h2>
+          <h2>Engineering Case</h2>
+        </div>
+        <ul>
+          <li v-for="(engCase,index) in caseList" :key="index">
+            <img class="case-img" :src="engCase.url">
+            <p>{{engCase.name}}</p>
           </li>
         </ul>
         <div class="swiper-pagination"></div>
@@ -74,6 +73,55 @@
           {url: 'http://www.njhuasheng.cn/images/up_images/201891413444.jpg'},
           {url: 'http://www.njhuasheng.cn/images/up_images/20189521445.jpg'},
           {url: 'http://www.njhuasheng.cn/images/up_images/201891413142.jpg'},
+        ],
+        productList: [
+          {
+            name: '陶粒',
+            url: 'http://www.njhuasheng.cn/images/up_images/2018952250.jpg',
+          },
+          {
+            name: '岩棉板',
+            url: 'http://www.njhuasheng.cn/images/up_images/201895221612.jpg',
+          },
+          {
+            name: '挤塑板',
+            url: 'http://www.njhuasheng.cn/images/up_images/20189522197.jpg',
+          },
+          {
+            name: '陶粒加气块',
+            url: 'http://www.njhuasheng.cn/images/up_images/201895222211.jpg',
+          },
+        ],
+        caseList:[
+          {
+            url: 'http://www.njhuasheng.cn/images/up_images/2018914134214.jpg',
+            name:'恒大建筑合作案例'
+          },
+          {
+            url: 'http://www.njhuasheng.cn/images/up_images/2018914133727.jpg',
+            name:'鑫科建筑合作案例'
+          },
+          {
+            url: 'http://www.njhuasheng.cn/images/up_images/201891413359.jpg',
+            name:'施工现场产品验收'
+          },
+          {
+            url: 'http://www.njhuasheng.cn/images/up_images/2018914133413.jpg',
+            name:'鲁南酒店合作案例'
+          },
+          {
+            url: 'http://www.njhuasheng.cn/images/up_images/2018914134327.jpg',
+            name:'水电四局合作案例'
+          },
+          {
+            url: 'http://www.njhuasheng.cn/images/up_images/201891413100.jpg',
+            name:'中建集团合作案例'
+          },
+
+
+
+
+
         ]
       }
     },
@@ -128,6 +176,11 @@
     clear: both;
   }
 
+  .container:before {
+    display: table;
+    content: " ";
+  }
+
   .title {
     margin-top: 80px;
     margin-bottom: 60px;
@@ -169,7 +222,7 @@
   .product .container ul li {
     float: left;
     width: 220px;
-    height: 220px;
+    height: 268px;
     margin-right: 106px;
     border: 1px solid #f5f5f5;
     box-shadow: 3.24px 2.35px 10px 0 rgba(0, 0, 0, .1);
@@ -189,13 +242,6 @@
     margin: 12px 0;
   }
 
-  .product .container ul li p {
-    color: #666;
-    font-size: 12px;
-    line-height: 20px;
-    padding: 0 13px;
-  }
-
   .product .container ul li:last-child {
     margin-right: 0;
   }
@@ -206,5 +252,47 @@
     height: 0;
     clear: both;
     visibility: hidden;
+  }
+
+  .product-img {
+    width: 100%;
+    height: 220px;
+  }
+
+  .case {
+    height: 782px;
+    background: #f7f7f7;
+  }
+
+  .case ul {
+    clear: both;
+    zoom: 1;
+    overflow: hidden;
+  }
+
+  .case ul li:nth-child(1), .case ul li:nth-child(2), .case ul li:nth-child(3) {
+    border-bottom: 1px solid #d2d2d2;
+    border-right: 1px solid #d2d2d2;
+  }
+
+  .case ul li:nth-child(4), .case ul li:nth-child(5) {
+    border-right: 1px solid #d2d2d2;
+  }
+
+  .case ul li:nth-child(3) {
+    border-right: 0;
+  }
+
+  .case ul li {
+    float: left;
+    width: 400px;
+    height: 260px;
+    padding: 36px 68px;
+    text-align: center;
+  }
+
+  .case-img{
+    width: 100%;
+    height: 175px;
   }
 </style>
